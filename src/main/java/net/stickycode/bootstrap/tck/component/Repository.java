4aafc46e3fbfbbd6350330f3f2cfc3654e854138
Michael McCallum <michael@StickyCode.net>
2015-09-11 08:23:25 +1200

    add debugging to show the component annotations that are actually being used, which need to be scanned to be resolved as the scanning is pre class load

--- src/main/java/net/stickycode/bootstrap/tck/component/Repository.java
+++ src/main/java/net/stickycode/bootstrap/tck/component/Repository.java
@@ -1,7 +1,9 @@
 package net.stickycode.bootstrap.tck.component;
 
+import net.stickycode.stereotype.StickyFramework;
 import net.stickycode.stereotype.component.StickyRepository;
 
+@StickyFramework
 @StickyRepository
 public class Repository {
 
